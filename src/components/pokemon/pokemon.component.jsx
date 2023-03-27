b02f5e36adc93018574fbf8eb5ebf089887b3e84
Héléna Younes <younesq.helena@gmail.com>
2023-03-27 02:27:46 +0200

    forgot errors

--- src/components/pokemon/pokemon.component.jsx
+++ src/components/pokemon/pokemon.component.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import { useParams} from 'react-router-dom';
 import {useState, useEffect} from 'react';
 import {CCard, CCardImage, CCardBody, CCardText, CCardTitle, CButton} from '@coreui/react';
@@ -41,57 +41,3 @@ const Pokemon = () => {
 }
 
 export default Pokemon;
-=======
-import { useState, useEffect, useLayoutEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import {CCard, CCardImage, CCardBody, CCardText, CCardTitle, CButton} from '@coreui/react';
-import './pokemon.css';
-
-const PokemonDetail = () => {
-
-  const [details, setDetails] = useState({});
-  const [isLoading, setIsLoading] = useState(true);
-  const { id } = useParams();
-
-  useEffect(()=> {
-    fetchPokemon()
-  },[]);
-
-// useLayoutEffect(() => {
-//   fetchPokemon();
-// }, []);
-
-  const fetchPokemon = async () => {
-    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-    const json = await res.json();
-    setDetails(json);
-    setIsLoading(false);
-  }
-  return (
-    <div className='card-container'>
-      {isLoading
-           ? <p>Loading</p>
-           : <CCard style={{ width: '18rem' }}>
-              <CCardImage orientation="top" src={details.sprites.front_default} />
-              <CCardBody>
-                <CCardTitle>{details.name}</CCardTitle>
-                <CCardText>
-                  Some quick example text to build on the card title and make up the bulk of the card's content.
-                </CCardText>
-                <CButton href="#">Go somewhere</CButton>
-              </CCardBody>
-            </CCard>
-        }
-
-    </div>
-  );
-}
-
-export default PokemonDetail;
-
-{/* <header>
-  <h2>{details.name}</h2>
-  {console.log(details)}
-  <img src={details.sprites.front_default} />
-</header> */}
->>>>>>> 51d197908ca9ea4b8703325c4d153b976b48f852
